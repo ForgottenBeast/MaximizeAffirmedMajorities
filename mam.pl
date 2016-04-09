7d48b6e2f2d7ea5433804f3b3d5282cf40008984
Quentin Mallet <quentin.mallet@gmail.com>
2016-04-09 18:07:11 +0200

    move genvote to mam

--- mam.pl
+++ mam.pl
@@ -4,6 +4,41 @@ use warnings;
 use autodie;
 use Data::Dumper;
 
+sub del_ballots{
+	for my $f(glob("*.vt")){
+		unlink $f;
+	}
+}
+
+sub make_ballots{
+	my $nb = shift;
+
+	sub complete{
+		my $hash = shift;
+		my %h = %$hash;
+		for my $k (keys %h){
+			if($h{$k} == 0){
+				return 0;
+			}
+		}
+		return 1;
+	}
+
+	for my $i (0 .. $nb){
+		open(my $fh, '>',$i.".vt");
+		my %hash = (1=>0,2=>0,3=>0,4=>0);
+		while(!complete(\%hash)){
+			my $vote = int(rand(4)) +1;
+			print "got vote = $vote\n";
+			if($hash{$vote} == 0){
+				print $fh "$vote\n";
+				$hash{$vote} = 1;
+			}
+		}
+		print "completed hash for $i\n";
+		close($fh);
+	}
+}
 sub enumerate{
 	my $candidates = shift;
 	my $votehash = {};
@@ -76,19 +111,25 @@ sub calculate_majorities{
 	return $majorities;
 }
 
-my @candidates = (1,2,3,4);
-my $hash = enumerate(\@candidates);
-my @files = glob("*.vt");
-foreach my $f (@files){
-	print "opening $f\n";
-	open(my $fh,'<',$f);
-	readfile($hash,$fh);
-	close($fh);
+sub main{
+	my @candidates = (1,2,3,4);
+	my $hash = enumerate(\@candidates);
+	my @files = glob("*.vt");
+	foreach my $f (@files){
+		print "opening $f\n";
+		open(my $fh,'<',$f);
+		readfile($hash,$fh);
+		close($fh);
+	}
+
+	my @maj = @{calculate_majorities($hash)};
+	my $i = 0;
+	while($i < $#maj-3){
+		print("$maj[$i] wins over $maj[$i+1] by $maj[$i+2]\n");
+		$i += 3;
+	}
 }
 
-my @maj = @{calculate_majorities($hash)};
-my $i = 0;
-while($i < $#maj-3){
-	print("$maj[$i] wins over $maj[$i+1] by $maj[$i+2]\n");
-	$i += 3;
-}
+make_ballots(100);
+main;
+del_ballots;
