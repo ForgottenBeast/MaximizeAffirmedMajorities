afe185f86105d8230b5966add3dfff68eaf4b77b
Quentin Mallet <quentin.mallet@gmail.com>
2016-04-09 18:55:05 +0200

    semi working mam

--- mam.pl
+++ mam.pl
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use autodie;
 use Data::Dumper;
+use List::Util qw(shuffle);
 
 sub del_ballots{
 	for my $f(glob("*.vt")){
@@ -11,31 +12,20 @@ sub del_ballots{
 }
 
 sub make_ballots{
-	my $nb = shift;
-
-	sub complete{
-		my $hash = shift;
-		my %h = %$hash;
-		for my $k (keys %h){
-			if($h{$k} == 0){
-				return 0;
-			}
-		}
-		return 1;
+	my ($nb,$nbc) = @_;
+	my @candidates;
+	my $j = 0;
+	for my $i (1..$nbc){
+		$candidates[$j] = $i;
+		$j++;
 	}
 
 	for my $i (0 .. $nb){
 		open(my $fh, '>',$i.".vt");
-		my %hash = (1=>0,2=>0,3=>0,4=>0);
-		while(!complete(\%hash)){
-			my $vote = int(rand(4)) +1;
-			print "got vote = $vote\n";
-			if($hash{$vote} == 0){
-				print $fh "$vote\n";
-				$hash{$vote} = 1;
-			}
+		my @ballot = shuffle(@candidates);
+		foreach my $c(@ballot){
+			print $fh "$c\n";
 		}
-		print "completed hash for $i\n";
 		close($fh);
 	}
 }
@@ -72,49 +62,51 @@ sub readfile{
 sub calculate_majorities{
 	my $votes = shift;
 	my @candidates = keys %$votes;
-	my $majorities = [];
-	my $k = 0;
+	my %majorities;
+	my %done;
 	for my $i (0 .. $#candidates){
 		for my $j (0 .. $#candidates){
 			my $c1 = $candidates[$i];
 			my $c2 = $candidates[$j];
-			if($c1 != $c2){
-				print "checking for candidates $c1 and $c2\n";
+			if($c1 != $c2 && !exists($done{$c1}{$c2}) && !exists($done{$c2}{$c1})){
+				$done{$c1}{$c2} = 1;
 				my $fori = $votes->{$c1}->{$c2};
 				my $forj = $votes->{$c2}->{$c1};
-				print "$c1 sur $c2 = $fori\n$c2 sur $c1 = $forj\n\n";
 				if($fori == 0){
-					$majorities->[$k] = $c2;
-					$majorities->[$k+1] = $c1;
-					$majorities->[$k+2] = 100;
+					$majorities{$c2}{$c1} = 100;
 				}
 				elsif($forj == 0){
-					$majorities->[$k] = $c1;
-					$majorities->[$k+1] = $c2;
-					$majorities->[$k+2] = 100;
+					$majorities{$c1}{$c2} = 100;
 				}
 				if($fori > $forj){
-					$majorities->[$k] = $c1;
-					$majorities->[$k+1] = $c2;
-					$majorities->[$k+2] = ($forj/$fori)*100;
-					$k += 3;
+					$majorities{$c1}{$c2} = ($forj/$fori)*100;
 				}
 				else{
-					$majorities->[$k] = $c2;
-					$majorities->[$k+1] = $c1;
-					$majorities->[$k+2] = ($fori/$forj)*100;
-					$k += 3;
+					$majorities{$c2}{$c1} = ($fori/$forj)*100;
 				}
 			}
 		}
 	}
-	return $majorities;
+	return \%majorities;
+}
+
+sub sort_maj{
+	my $majs = shift;
+
 }
 
 sub main{
-	my @candidates = (1,2,3,4);
+	my $nbc = shift;
+	my @candidates;
+	my $j = 0;
+	for my $i (1..$nbc){
+		$candidates[$j] = $i;
+		$j++;
+	}
+
+	print "candidates:\n".Dumper(\@candidates);
 	my $hash = enumerate(\@candidates);
-	my @files = glob("*.vt");
+	my @files = shuffle(glob("*.vt"));
 	foreach my $f (@files){
 		print "opening $f\n";
 		open(my $fh,'<',$f);
@@ -122,14 +114,15 @@ sub main{
 		close($fh);
 	}
 
-	my @maj = @{calculate_majorities($hash)};
-	my $i = 0;
-	while($i < $#maj-3){
-		print("$maj[$i] wins over $maj[$i+1] by $maj[$i+2]\n");
-		$i += 3;
-	}
+	my %maj = %{calculate_majorities($hash)};
+	print Dumper(\%maj);
 }
 
-make_ballots(100);
-main;
+if(!defined($ARGV[0])||!defined($ARGV[1])){
+	print 'usage: ./mam.pl $number_of_candidates $number_of_ballots'."\n";
+	exit(0);
+}
+
+make_ballots($ARGV[1],$ARGV[0]);
+main($ARGV[0]);
 del_ballots;
