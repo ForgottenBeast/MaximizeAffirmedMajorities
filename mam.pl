047e6ef9768135786e363c7a4b859976c710cbcf
Quentin Mallet <quentin.mallet@gmail.com>
2016-04-10 11:26:04 +0200

    now working on tiebreak with multiple rankings

--- mam.pl
+++ mam.pl
@@ -16,6 +16,7 @@ sub del_ballots{
 sub make_ballots{
 	my ($nb,$nbc) = @_;
 	my @candidates;
+	my $same_rank = "";
 	my $j = 0;
 	for my $i (1..$nbc){
 		$candidates[$j] = $i;
@@ -23,10 +24,27 @@ sub make_ballots{
 	}
 
 	for my $i (0 .. $nb){
-		open(my $fh, '>',$i.".vt");
+		my $same_rank = "";
 		my @ballot = shuffle(@candidates);
+		open(my $fh, '>',$i.".vt");
 		foreach my $c(@ballot){
-			print $fh "$c\n";
+			if(int(rand(6)) == 0){#leave all other candidates out
+				print STDERR "leaving out in $i.vt\n";
+				last;
+			}
+			elsif(int(rand(2)) == 0){#put one or more candidate on this row
+				if($same_rank eq ""){
+					$same_rank = $c;
+					next;
+				}
+				else{
+					print $fh "$c,$same_rank";
+					print STDERR "putting candidates together in $i.vt\n";
+				}
+			}
+			else{
+				print $fh "$c\n";
+			}
 		}
 		close($fh);
 	}
@@ -44,27 +62,177 @@ sub enumerate{
 	return $votehash;
 }
 
+sub vote_against_below{
+	my ($startidx,$cv,$votes,$candidate) = @_;
+	my $c = $candidate;
+	my @curvote = @{$cv};
+	print STDERR "working on candidate $c\n";
+	for my $j($startidx .. $#curvote){
+
+	#if on the lines below I encounter multiple candidates
+	#I count a vote against all of them
+		if($curvote[$j] =~ /^\d,\d/){
+			print STDERR "loser below is multiple\n";
+			my @losers = split /,/,$curvote[$j];
+			print STDERR @losers;
+			foreach my $l (@losers){
+				print STDERR "add vote for $c against $l\n";
+				$votes->{$c}->{$l}++;
+			}
+		}
+		
+		#if it is a normal case then I deal with it
+		else{
+			print STDERR "add vote for $c against $curvote[$j]\n";
+			$votes->{$c}->{$curvote[$j]}++;
+		}
+	}
+}
+
 sub readfile{
 	my ($votes,$fh) = @_;
 	my $i = 0;
 	my @curvote;
+
+	#prepare a hash to check for left out candidates
+	print STDERR "\n\nseen setup\n";
+	my %seen;
+	my @keys = keys %{$votes};
+	foreach my $k (@keys){
+		print STDERR "adding key $k\n";
+		$seen{$k} = 0;
+	}
+	print STDERR "seen setup done \n\n\n";
 	while(<$fh>){
 		chomp $_;
 		$curvote[$i] = $_;
+		if($curvote[$i] =~ /\A\d,/){
+			print STDERR "got a match on $curvote[$i]\n";
+			my @votes = split /,\s?/, $curvote[$i];
+			print STDERR "got multi votes: @votes";
+			foreach my $v(@votes){
+				print STDERR "adding multivote key $v to seen\n";
+				$seen{$v} = 1;
+			}
+		}
+		else{
+			$seen{$curvote[$i]} = 1;
+		}
 		$i++;
 	}
-	for my $i (0 .. $#curvote){
-	our $tiebreak_ready;
-		if(!$tiebreak_ready){
-			our @tiebreak;
-			push @tiebreak,$curvote[$i];
-			my @cdt = keys %{$votes};
-			$tiebreak_ready = $#tiebreak == $#cdt;
-		}
-		for my $j ($i+1 .. $#curvote){
-			$votes->{$curvote[$i]}->{$curvote[$j]}++;
+	print STDERR "curvote as of end of file:\n";
+	foreach my $v (@curvote){
+		print STDERR "$v\n";
+	}
+
+	#all candidates left out of the ballot are accounted for as if they
+	#were added together at the lowest possible position
+	my $leftovers = "";
+	foreach my $k (@keys){
+		if(!$seen{$k}){
+			print STDERR "I have not seen $k yet, adding to leftover\n";
+			if($leftovers =~ /\d\z/){
+				$leftovers = $leftovers . ",$k";
+			}
+			else{#leftovers is empty
+				$leftovers = $k;
+			}
 		}
 	}
+	if($leftovers ne ""){
+		print STDERR "got leftover string: $leftovers\n";
+		$curvote[$i] = $leftovers;
+	}
+
+
+	print STDERR "\nafter adding leftovers, curvote = \n";
+	foreach my $v(@curvote){
+		print STDERR "$v\n";
+	}
+
+	for my $i (0 .. $#curvote){
+		my @candidates;
+		if($curvote[$i] =~ /^\d,\s?\d/){#more than one candidate on this line
+			print STDERR "got a match on skivvy curvote: $curvote[$i]\n";
+			#I split the candidate list into an array
+			@candidates = split /,\s?/,$curvote[$i];
+			print STDERR "splat it in @candidates\n";
+
+			#foreach candidate in the list I count one vote for him
+			#against everyone below him
+			foreach my $c (@candidates){
+				vote_against_below($i+1,\@curvote,$votes,$c);
+			}
+		}
+		else{
+			vote_against_below($i+1,\@curvote,$votes,$curvote[$i]);
+		}
+		our $tiebreak_ready;
+		if(!$tiebreak_ready){
+			update_tiebreak(\@curvote,$votes);
+		}
+	}
+}
+
+sub update_tiebreak{
+	my ($cv,$votes)= @_;
+	my @curvote = @{$cv};
+	our( @tiebreak,$tiebreak_ready);
+
+	if(!@tiebreak){#empty tiebreak
+		print STDERR "empty tiebreak, pushing\n";
+		foreach my $v (@curvote){
+			push @tiebreak,$v;
+			print STDERR "$v\n";
+		}
+		print STDERR "\n"x3;
+	}
+	else{
+		foreach my $t (@tiebreak){
+			if($t =~ /\d,\d/){
+				print STDERR "we have a tiebreak issue: @tiebreak\n";
+				my @candidates = split /,\s?/, $t;
+				print STDERR "tiebreak candidates = @candidates\n";
+				for my $i (0 .. $#candidates){
+					for my $j (0 .. $#candidates){
+						if($i != $j){
+							#lets check if the current vote can solve
+							#the tiebreak equality issue
+							print STDERR "working on tiebreak candidate $candidates[$i]
+							and $candidates[$j]\n";
+							my $idx = firstidx {$_ =~ /$candidates[$i]/} @curvote;
+							my $jdx = firstidx {$_ =~ /$candidates[$j]/} @curvote;
+							my $curpos = firstidx {$_ eq $t} @tiebreak;
+							print STDERR "issue with $t at $curpos\n";
+							if($idx == -1 || $jdx == $idx || $jdx == -1){
+								#the vote itself is tied
+								print STDERR "vote is tied\n";
+								last;
+							}
+							if($idx < $jdx){
+								print STDERR "ci is before cj\n";
+								$tiebreak[$curpos] = $j;
+								splice @tiebreak,$curpos,0,$candidates[$i];
+								#insert i before j
+							}
+							else{
+								print STDERR "ci is after cj\n";
+								$tiebreak[$curpos] = $i;
+								splice @tiebreak,$curpos,0,$candidates[$j];
+								#insert j before i
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	print STDERR "tiebreak currently = \n";
+	print STDERR "@tiebreak\n";
+
+	my @cdt = keys %{$votes};
+	$tiebreak_ready = $#tiebreak == $#cdt;#ready when we have 
+	#one candidate per line
 }
 
 sub calculate_majorities{
@@ -121,12 +289,6 @@ sub win_order{
 				push @win,$a_subkey;
 			}
 		}
-
-=head
-ici, utiliser shift et unshift pour rajouter au dessus ou en dessous dans la
-liste, et first_index pour vérifier si un candidat est déjà dans la liste des
-gagnants
-=cut
 	}
 	print "Here is the win order:\n";
 	foreach my $c (@win){
@@ -232,6 +394,6 @@ if(!defined($ARGV[0])||!defined($ARGV[1])){
 
 our @tiebreak;
 our $tiebreak_ready = 0;
-make_ballots($ARGV[1],$ARGV[0]);
+#make_ballots($ARGV[1],$ARGV[0]);
 main($ARGV[0]);
-del_ballots;
+#del_ballots;
